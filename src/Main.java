c17dd7f4647aaaaeb9ab76ffa69a40c9ce72fe30
Ivan Umpelev <dpakoh.b.kedax@gmail.com>
2022-07-02 22:37:45 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,90 @@
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static int arabicCounter = 0;
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String s = "LIX / X";
+        System.out.println(calc(s));
     }
+
+    public static String calc(String input) throws Exception {
+        int result;
+        Scanner scanner = new Scanner(input);
+
+        int firstOperand = getOperand(scanner.next());
+        String operator = scanner.next();
+        int secondOperand = getOperand(scanner.next());
+
+        if (scanner.hasNext()) {
+            throw new Exception("формат математической операции не удовлетворяет заданию - " +
+                    "два операнда и один оператор (+, -, /, *)");
+        }
+
+
+        result = switch (operator) {
+            case "+" -> firstOperand + secondOperand;
+            case "-" -> firstOperand - secondOperand;
+            case "*" -> firstOperand * secondOperand;
+            case "/" -> firstOperand / secondOperand;
+            default -> throw new Exception("Неопределенный арифметический оператор");
+        };
+        if (arabicCounter == 0) {
+            if (result < 1) {
+                throw new Exception("Результат меньше еденицы и не может быть представлен римскими цифрами");
+            }
+            return Roman2ArabicConverter.toRoman(result);
+        } else if (arabicCounter == 2) {
+            return String.valueOf(result);
+        } else {
+            throw new Exception("Невозможно произвести вычисления над арабскими и римскими цифрами одновременно");
+        }
+    }
+
+    private static int getOperand(String operandString) throws Exception {
+
+
+        if (isArabicDigit(operandString)) {
+            arabicCounter++;
+            int operand = Integer.parseInt(operandString);
+
+            if (operand < 1) {
+                throw new Exception("Число меньше 1");
+            }
+
+            if (operand > 10) {
+                throw new Exception("Число больше 10");
+            }
+
+            return operand;
+
+        }
+
+        if (isRomanDigit(operandString)) {
+            int operand = Roman2ArabicConverter.toArabic(operandString.toUpperCase());
+            if (operand < 1) {
+                throw new Exception("Число меньше 1");
+            }
+
+            if (operand > 10) {
+                throw new Exception("Число больше 10");
+            }
+
+            return operand;
+        }
+
+        throw new Exception("Операнда не является числом. Операнда: " + operandString);
+
+    }
+
+    private static boolean isArabicDigit(String string) {
+        return string.matches("\\d+");
+    }
+
+    private static boolean isRomanDigit(String string) {
+        return string.toUpperCase().matches("^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$");
+    }
+
 }
